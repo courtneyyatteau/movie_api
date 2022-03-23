ae25ba35a846341234719e4d44324cf01ab1884f
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-03-22 21:53:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.get(
   "/movies/releaseYear/:Year",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Movies.find({ ReleaseYear: req.params.Year })
+    Movies.find({ Year: req.params.Year })
       .then((movie) => {
         if (movie) {
           res.json(movie);
