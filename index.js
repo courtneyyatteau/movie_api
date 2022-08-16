c46040264f96830a1b54e3146ffb46d881ed9670
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-08-16 08:43:32 -0400

    revert

--- index.js
+++ index.js
@@ -14,26 +14,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
-let allowedOrigins = ["http://localhost:1234", "http://localhost:4200"];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
+
 
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
