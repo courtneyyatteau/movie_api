5f61cb9658a31bc05af8f68d085ea68bb8c6e91a
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-08-16 08:57:23 -0400

    express static

--- index.js
+++ index.js
@@ -14,11 +14,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(express.static("public"));
+app.use(cors());
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
