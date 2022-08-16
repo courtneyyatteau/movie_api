3a6d23c5254fd3f7ea9d0ff4bc1c65a0a7d9ffe6
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-08-16 08:51:45 -0400

    try origin *

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
-
 
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
