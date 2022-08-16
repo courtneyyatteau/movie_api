96c1b13dec80f66c8cdaa259835a79373ae4974a
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-08-16 08:40:07 -0400

    local host update

--- index.js
+++ index.js
@@ -14,15 +14,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
-app.use(cors());
+let allowedOrigins = ["http://localhost:1234", "http://localhost:4200"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
-/*mongoose.connect("mongodb://localhost:27017/flixFolioDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});*/
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
